f49a903f2b3d3a464e70f56f3f8d7db1c3a7a521
vimal-s <36556890+vimal-s@users.noreply.github.com>
2018-02-18 14:45:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/article-2', function (req, res) {
 });
 
 app.get('/article-3', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-3.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'article-3'));
 });
 
 app.get('/ui/style.css', function (req, res) {
