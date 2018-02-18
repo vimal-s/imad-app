7c0d5dd9c5ea015291c0a159e4d63fd58f3fd9ec
vimal-s <36556890+vimal-s@users.noreply.github.com>
2018-02-18 14:44:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/article-1', function (req, res) {
 });
 
 app.get('/article-2', function (req, res) {
-    res.send('Article two is requested, will be served here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-3', function (req, res) {
-    res.send('Article three is requested, will be served here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-3.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
