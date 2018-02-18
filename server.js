2df66a3eb1fc2e70dfe4f8d7e47c2ba5d851c134
vimal-s <36556890+vimal-s@users.noreply.github.com>
2018-02-18 16:32:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,65 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article One',
+    date: 'Feb 18,2018',
+    heading: 'Article One',
+    content: `  <p>
+                    This is the content for my first article.
+                </p>
+                <p>
+                    This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                </p>
+                <p>
+                    This is the content for my first article.Bingo!!!!!!
+                </p>`
+};
+
+function createTemplate (data) {
+    var title = data.title;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = `<html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name='viewport' content="width=device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+            <style>
+                
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href='/'>Home</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    articleOne.date;
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-1', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-1.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-2', function (req, res) {
