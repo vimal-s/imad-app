2b859c2ea82f2284b67325ffcea0f739f8fcbde2
vimal-s <36556890+vimal-s@users.noreply.github.com>
2018-02-23 09:23:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,6 +16,6 @@ img.onclick = function() {
     var interval = setInterval(moveRight, 100);
     
 };*/
-im.onClick = function() {
+img.onClick = function() {
     img.style.marginLeft = '100px';
 };
