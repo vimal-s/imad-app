aa67f0caffffdc8daca7f08592eaac1af9dc7812
vimal-s <36556890+vimal-s@users.noreply.github.com>
2018-02-23 09:07:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 
 //Move theImage on click
-/*var img = document.getElementById('madi');
+var img = document.getElementById('madi');
 img.onclick = function() {
     img.style.marginLeft = '100px';
-};*/
+};
