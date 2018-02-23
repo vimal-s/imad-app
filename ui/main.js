e95f4f45b117dea040f8ed387690d7a02668679b
vimal-s <36556890+vimal-s@users.noreply.github.com>
2018-02-23 09:15:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,12 @@ element.innerHTML = 'New Value';
 
 //Move theImage on click
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft =marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function() {
-    img.style.marginLeft = '100px';
+    var interval = setinterval(moveRight, 100);
+    
 };
