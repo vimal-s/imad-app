3c7f89e18e311601d4fa8ed4ef88d319bee3b29f
Bing Ding <ding@bing.com>
2018-02-24 20:00:00 +0530

    Local change to main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,33 @@
-console.log('Loaded!');
+//Counter code
+let button = document.getElementById('counter');
+
+//var counter = 0;
+
+button.onclick = function () {
+	//Create a request object
+	var request = new XMLHttpRequest();
+	
+	//Capture the response and store it in a variable
+	request.onreadystatechange = function () {
+		if (request.readyState === XMLHttpRequest.DONE) {
+			//Take some action
+			if (request.status === 200) {
+				var counter = request.responseText;
+				var span = document.getElementById('count');
+				span.innerHTML = counter.toString();
+			}
+		}
+		//Not don yet
+	};
+	//Render the variable in the correct span
+	//counter = counter + 1;
+	
+	//Make a request
+	request.open('GET', 'http://127.0.0.1:80/counter', true);
+	request.send(null);
+};
+
+/*console.log('Loaded!');
 
 //Change the text of the main-text div
 var element = document.getElementById('main-text');
