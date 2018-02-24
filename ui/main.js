b3658ce30d80b1bcc5784a88b659a80c1226f1ab
vimal-s <36556890+vimal-s@users.noreply.github.com>
2018-02-24 22:21:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -57,7 +57,7 @@ submit.onclick = function () {
 	var nameInput = document.getElementById('name');
 var name = nameInput.value;
 
-	request.open('GET', 'http://vimalsingh992.imad.harura-app.io/submit-name?name=' + name, true);
+	request.open('GET', 'http://vimalsingh992.imad.hasura-app.io/submit-name?name=' + name, true);
 	request.send(null);
 };
 
