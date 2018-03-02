8cbf6af1f9b13bc1c20f91fd53e6ab54ff40d097
vimal-s <36556890+vimal-s@users.noreply.github.com>
2018-03-02 15:02:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database: 'vimalsingh992',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.env.db_password
+    password: process.env.DB_PASSWORD
 }
 
 var app = express();
