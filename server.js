d547ef96c18c6123bffd020a0c75727b0a35435d
vimal-s <36556890+vimal-s@users.noreply.github.com>
2018-03-02 14:32:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ var pool = new Pool(config);
 app.get('/test-db', function(req, res) {
    //make a select request
    //return a response with the results
-   pool.query('select * FROM test', function(err, result) {
+   pool.query('SELECT * FROM test', function(err, result) {
        if (err) {
            res.status(500).send(err.toString());
        } else {
