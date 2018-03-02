65768aad999d9229be6cf4a2ade8492bc386405b
vimal-s <36556890+vimal-s@users.noreply.github.com>
2018-03-02 14:13:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-//var pool = require('pg').pool;
+var pool = require('pg').pool;
 
 /*var config = {
     user: 'vimalsingh992',
