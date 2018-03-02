fc2670ae028ecded03f6d2373542dcaf84361588
vimal-s <36556890+vimal-s@users.noreply.github.com>
2018-03-02 14:19:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.get('/test-db', function(req, res) {
        if (err) {
            res.status(500).send(err.toString());
        } else {
-           res.send(JSON.stringify(result));
+           res.send(json.stringify(result));
        }
    });
 });
