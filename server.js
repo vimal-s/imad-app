4939afd39c4303cd195fcec5b22e47d9bdfdd2f0
vimal-s <36556890+vimal-s@users.noreply.github.com>
2018-03-02 14:58:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,10 +131,20 @@ app.get('/submit-name', function(req, res) {//URL: /submit-name?name=xxxxx
 });
 
 
-app.get('/:articleName', function (req, res) {		//Doubt here
+app.get('/articles/:articleName', function (req, res) {		//Doubt here
     //articleName = article-one
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function (err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            if (result.rows.length == 0) {
+                res.status(404).send('Article not fnd');
+            } else {
+                var articleDate = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
 });
 
 app.get('/ui/style.css', function (req, res) {
