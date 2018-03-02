e3c18589ef60a7c8bac33e7f516383a545db30d2
vimal-s <36556890+vimal-s@users.noreply.github.com>
2018-03-02 17:08:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ function createTemplate (data) {
                     ${heading}
                 </h3>
                 <div>
-                    ${date};
+                    ${date}
                 </div>
                 <div>
                     ${content}
