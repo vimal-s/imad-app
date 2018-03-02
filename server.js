ddae5aaab5c3cd053f8927f85aa52e8de902129d
vimal-s <36556890+vimal-s@users.noreply.github.com>
2018-03-02 17:04:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ app.get('/articles/:articleName', function (req, res) {		//Doubt here
             if (result.rows.length === 0) {
                 res.status(404).send('Article not fnd');
             } else {
-                var articleDate = result.rows[0];
+                var articleData = result.rows[0];
                 res.send(createTemplate(articleData));
             }
         }
