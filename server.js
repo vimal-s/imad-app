30163405eb5b44b5c616370a5553887cfeac889a
vimal-s <36556890+vimal-s@users.noreply.github.com>
2018-03-03 16:40:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.get('/', function (req, res) {
 });
 
 function hash(input, salt) {
-    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 2, 'sha512');
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return hashed.toString('hex');
 }
 
