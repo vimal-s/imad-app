9735f07ec02f0d608be1cfc39f91f51c67c5713e
vimal-s <36556890+vimal-s@users.noreply.github.com>
2018-03-04 12:06:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,8 +113,8 @@ app.get('/hash/:input', function(req, res) {
     var hashedString = hash(req.params.input, 'this-is-some-random-string');
     res.send(hashedString);
 });
-/*
-app.get('/create-user', function(req, res) {
+
+app.post('/create-user', function(req, res)  {
    //username, password
    //JSON
    var username = req.body.username;
@@ -129,7 +129,7 @@ app.get('/create-user', function(req, res) {
        }
    });
 });
-*/
+
 var pool = new Pool(config);
 app.get('/test-db', function(req, res) {
    //make a select request
