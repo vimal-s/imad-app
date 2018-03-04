e98de3738616e7b803dcd6f45eafc8a6da89f94b
vimal-s <36556890+vimal-s@users.noreply.github.com>
2018-03-04 13:25:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -130,6 +130,30 @@ app.post('/create-user', function(req, res)  {
    });
 });
 
+app.post('/login', function (req, res) {
+   var username = req.body.username;
+   var password = req.body.password;
+   pool.query('SELECT * FROM "user" username = $1', [username], function(err, result) {
+       if (err) {
+           res.status(500).send(err.toString());
+       } else {
+           if (result.rows.length === 0) {
+               res.send(403).send('username/password is invalid');
+           } else {
+               //Password matching here
+               var dbString = result.rows[0].password;
+               var salt = dbString.split('$')[2];
+               var hashedPassword = hash(password, salt);
+               if (hashedPassword === dbString) {
+                   res.send('Credentials correct!');
+               } else {
+                   res.send(403).send('INVALIDDDDDDDD');
+               }
+           }
+       }
+   }); 
+});
+
 var pool = new Pool(config);
 app.get('/test-db', function(req, res) {
    //make a select request
