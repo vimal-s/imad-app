01527f52d082f8cbcf47d482f0034ea7d7584a26
vimal-s <36556890+vimal-s@users.noreply.github.com>
2018-03-04 14:07:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,30 +3,6 @@ let button = document.getElementById('counter');
 
 //var counter = 0;
 
-button.onclick = function () {
-	//Create a request object
-	var request = new XMLHttpRequest();
-	
-	//Capture the response and store it in a variable
-	request.onreadystatechange = function () {
-		if (request.readyState === XMLHttpRequest.DONE) {
-			//Take some action
-			if (request.status === 200) {
-			    var counter = request.responseText;
-				var span = document.getElementById('count');
-				span.innerHTML = counter.toString();
-			
-			}
-		}
-		//Not done yethttp://localhost/
-	};
-	//Render the variable in the correct span
-	//counter = counter + 1;
-	
-	//Make a request
-	request.open('GET', 'http://vimalsingh992.imad.hasura-app.io/counter', true);
-	request.send(null);
-};
 
 // Submit username/password to login
 var submit = document.getElementById('submit_btn');
