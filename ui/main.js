daa35d55657f01819ce31e95790b92ec26483643
vimal-s <36556890+vimal-s@users.noreply.github.com>
2018-03-06 22:08:19 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,64 +1,107 @@
-//Counter code
-let button = document.getElementById('counter');
 
-//var counter = 0;
+Skip to content
+This repository
+
+    Pull requests
+    Issues
+    Marketplace
+    Explore
+
+    @vimal-s
+
+1
+0
+
+    8,302
+
+29abhisheksoni/imad-app forked from hasura/imad-app
+Code
+Pull requests 0
+Projects 0
+Wiki
+Insights
+imad-app/ui/main.js
+8fa4245 22 hours ago
+@29abhisheksoni 29abhisheksoni [imad-console] Updates ui/main.js
+@29abhisheksoni
+@shahidhk
+64 lines (53 sloc) 1.78 KB
+//var button = document.getElementById('counterr');
 
 
-// Submit username/password to login
+
+//function exec()
+//{
+
+//var request = new XMLHttpRequest();
+//request.onreadystatechange = function() 
+  //  {
+    //    if (request.readyState === XMLHttpRequest.DONE) 
+      //      {
+        //      //Take Some Action
+          //    if (request.status === 200) 
+            //    { 
+              //      var counter = request.responseText; 
+                //    var span = document.getElementById('count');
+                  //  span.innerHTML = counter.toString();
+            //    } 
+        //    } 
+    //}; 
+//request.open("GET", "http://u29abhisheksoni.imad.hasura-app.io/counter", true);
+//request.send(); 
+//console.log('EXECUTED');
+//}
+
+
+//submit username password to login
 var submit = document.getElementById('submit_btn');
-submit.onclick = function () {
-	//MAKE A request to the server and send the name
-	
-	//Create a request object
-	var request = new XMLHttpRequest();
-	
-	//Capture the response and store it in a variable
-	request.onreadystatechange = function () {
-		if (request.readyState === XMLHttpRequest.DONE) {
-			//Take some action
-				if (request.status === 200) {
-				    alert('login successfull');
-				} else if (request.status === 403) {
-				    alert('incorrect');
-				} else if (request.status === 500) {
-				    alert('server error');
-				}
-			}
-		
-		//Not done yethttp://localhost/
-	};
-	//Render the variable in the correct span
-	//counter = counter + 1;
-	
-	//Make a request
-	var username = document.getElementById('username').value;
-	var password = document.getElementById('password').value;
-	console.log(username);
-	console.log(password);
-	
-	request.open('POST', 'http://vimalsingh992.imad.hasura-app.io/login', true);
-	request.setRequestHeader('Content-Type', 'application/json');
-	request.send(JSON.stringify({username: username, password: password}));
-};
-
-/*console.log('Loaded!');
-
-//Change the text of the main-text div
-var element = document.getElementById('main-text');
-
-element.innerHTML = 'New!!!!!!!!!!!!!!!!!!!!!! Value';
-
-//Move theImage on click
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft =marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight, 100);
+submit.onclick = function()
+{
     
-};
-/*img.onclick = function() {
-    img.style.marginLeft = '100px';
-};*/
+    var request = new XMLHttpRequest();
+   
+   request.onreadystatechange = function()
+   {
+       if(request.readystate === XMLHttpRequest.DONE)
+       {
+           if(request.status === 200)
+           {
+             console.log('user logged in');
+             alert("logged in sucessfully");
+           }
+           else if (request.status === 403)
+           {
+             alert("username/password is incorrect");
+           }
+            else if (request.status === 500)
+            {
+              alert("something went wrong on server");
+            }
+          
+       }
+   };
+   var username =  document.getElementById('username');
+    var password =  document.getElementById('password');
+    console.log(username);
+    console.log(password);
+   request.open('POST', 'https://u29abhisheksoni.imad.hasura-app.io/login', true);
+   request.setRequestHeader('Content-Type','application/json');
+   request.send(JSON.stringify({username: username, password: password}));
+   
+   
+ };
+
+    © 2018 GitHub, Inc.
+    Terms
+    Privacy
+    Security
+    Status
+    Help
+
+    Contact GitHub
+    API
+    Training
+    Shop
+    Blog
+    About
+
